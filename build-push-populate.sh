d71bc68a61b675c1c57a50e345de5808dc939c69
Lianhao Lu <lianhao.lu@intel.com>
2024-09-18 04:22:16 +0000

    build-push-populate.sh: Adjust due to dir layout change in upstream
    Signed-off-by: Lianhao Lu <lianhao.lu@intel.com>

--- build-push-populate.sh
+++ build-push-populate.sh
@@ -53,55 +53,55 @@ function add_image_data() {
 }
 
 # BEGINNING of adding image data
-add_image_data dataprep-redis $DIRNAME/GenAIComps comps/dataprep/redis/langchain/docker/Dockerfile
-add_image_data dataprep-qdrant $DIRNAME/GenAIComps comps/dataprep/qdrant/docker/Dockerfile
-add_image_data dataprep-redis-llama-index $DIRNAME/GenAIComps comps/dataprep/redis/llama_index/docker/Dockerfile
-add_image_data dataprep-on-ray-redis $DIRNAME/GenAIComps comps/dataprep/redis/langchain_ray/docker/Dockerfile
+add_image_data dataprep-redis $DIRNAME/GenAIComps comps/dataprep/redis/langchain/Dockerfile
+add_image_data dataprep-qdrant $DIRNAME/GenAIComps comps/dataprep/qdrant/langchain/Dockerfile
+add_image_data dataprep-redis-llama-index $DIRNAME/GenAIComps comps/dataprep/redis/llama_index/Dockerfile
+add_image_data dataprep-on-ray-redis $DIRNAME/GenAIComps comps/dataprep/redis/langchain_ray/Dockerfile
 
-add_image_data asr $DIRNAME/GenAIComps comps/asr/Dockerfile
-add_image_data whisper $DIRNAME/GenAIComps comps/asr/whisper/Dockerfile
-add_image_data whisper-gaudi $DIRNAME/GenAIComps comps/asr/whisper/Dockerfile_hpu
+add_image_data asr $DIRNAME/GenAIComps comps/asr/whisper/Dockerfile
+add_image_data whisper $DIRNAME/GenAIComps comps/asr/whisper/dependency/Dockerfile
+add_image_data whisper-gaudi $DIRNAME/GenAIComps comps/asr/whisper/dependency/Dockerfile.intel_hpu
 
-add_image_data embedding-tei $DIRNAME/GenAIComps comps/embeddings/langchain/docker/Dockerfile
+add_image_data embedding-tei $DIRNAME/GenAIComps comps/embeddings/tei/langchain/Dockerfile
 
-add_image_data tts $DIRNAME/GenAIComps comps/tts/Dockerfile
-add_image_data speecht5 $DIRNAME/GenAIComps comps/tts/speecht5/Dockerfile
-add_image_data speecht5-gaudi $DIRNAME/GenAIComps comps/tts/speecht5/Dockerfile_hpu
+add_image_data tts $DIRNAME/GenAIComps comps/tts/speecht5/Dockerfile
+add_image_data speecht5 $DIRNAME/GenAIComps comps/tts/speecht5/dependency/Dockerfile
+add_image_data speecht5-gaudi $DIRNAME/GenAIComps comps/tts/speecht5/dependency/Dockerfile.intel_hpu
 
-add_image_data web-retriever-chroma $DIRNAME/GenAIComps comps/web_retrievers/langchain/chroma/docker/Dockerfile
+add_image_data web-retriever-chroma $DIRNAME/GenAIComps comps/web_retrievers/chroma/langchain/Dockerfile
 
 add_image_data llm-tgi $DIRNAME/GenAIComps comps/llms/text-generation/tgi/Dockerfile
-add_image_data llm-ollama $DIRNAME/GenAIComps comps/llms/text-generation/ollama/Dockerfile
-add_image_data llm-docsum-tgi $DIRNAME/GenAIComps comps/llms/summarization/tgi/Dockerfile
-add_image_data llm-faqgen-tgi $DIRNAME/GenAIComps comps/llms/faq-generation/tgi/Dockerfile
-add_image_data llm-vllm $DIRNAME/GenAIComps comps/llms/text-generation/vllm/docker/Dockerfile.microservice
-add_image_data llm-vllm-hpu $DIRNAME/GenAIComps comps/llms/text-generation/vllm/docker/Dockerfile.hpu
-add_image_data llm-vllm-ray $DIRNAME/GenAIComps comps/llms/text-generation/vllm-ray/docker/Dockerfile.microservice
-add_image_data llm-vllm-ray-hpu $DIRNAME/GenAIComps comps/llms/text-generation/vllm-ray/docker/Dockerfile.vllmray
+add_image_data llm-ollama $DIRNAME/GenAIComps comps/llms/text-generation/ollama/langchain/Dockerfile
+add_image_data llm-docsum-tgi $DIRNAME/GenAIComps comps/llms/summarization/tgi/langchain/Dockerfile
+add_image_data llm-faqgen-tgi $DIRNAME/GenAIComps comps/llms/faq-generation/tgi/langchain/Dockerfile
+add_image_data llm-vllm $DIRNAME/GenAIComps comps/llms/text-generation/vllm/langchain/Dockerfile
+add_image_data llm-vllm-hpu $DIRNAME/GenAIComps comps/llms/text-generation/vllm/langchain/dependency/Dockerfile.intel_hpu
+add_image_data llm-vllm-ray $DIRNAME/GenAIComps comps/llms/text-generation/vllm/ray/Dockerfile
+add_image_data llm-vllm-ray-hpu $DIRNAME/GenAIComps comps/llms/text-generation/vllm/ray/dependency/Dockerfile
 
-add_image_data guardrails-tgi $DIRNAME/GenAIComps comps/guardrails/llama_guard/docker/Dockerfile
-add_image_data guardrails-pii-detection $DIRNAME/GenAIComps comps/guardrails/pii_detection/docker/Dockerfile
+add_image_data guardrails-tgi $DIRNAME/GenAIComps comps/guardrails/llama_guard/langchain/Dockerfile
+add_image_data guardrails-pii-detection $DIRNAME/GenAIComps comps/guardrails/pii_detection/Dockerfile
 
-add_image_data retriever-redis $DIRNAME/GenAIComps comps/retrievers/langchain/redis/docker/Dockerfile
-add_image_data retriever-qdrant $DIRNAME/GenAIComps comps/retrievers/haystack/qdrant/docker/Dockerfile
+add_image_data retriever-redis $DIRNAME/GenAIComps comps/retrievers/redis/langchain/Dockerfile
+add_image_data retriever-qdrant $DIRNAME/GenAIComps comps/retrievers/qdrant/haystack/Dockerfile
 
-add_image_data reranking-tei $DIRNAME/GenAIComps comps/reranks/tei/docker/Dockerfile
+add_image_data reranking-tei $DIRNAME/GenAIComps comps/reranks/tei/Dockerfile
 
-add_image_data chatqna $DIRNAME/GenAIExamples/ChatQnA/docker Dockerfile
-add_image_data chatqna-guardrails $DIRNAME/GenAIExamples/ChatQnA/docker Dockerfile_guardrails
-add_image_data chatqna-ui $DIRNAME/GenAIExamples/ChatQnA/docker/ui docker/Dockerfile
-add_image_data chatqna-conversation-ui $DIRNAME/GenAIExamples/ChatQnA/docker/ui docker/Dockerfile.react
+add_image_data chatqna $DIRNAME/GenAIExamples/ChatQnA Dockerfile
+add_image_data chatqna-guardrails $DIRNAME/GenAIExamples/ChatQnA Dockerfile_guardrails
+add_image_data chatqna-ui $DIRNAME/GenAIExamples/ChatQnA/ui docker/Dockerfile
+add_image_data chatqna-conversation-ui $DIRNAME/GenAIExamples/ChatQnA/ui docker/Dockerfile.react
 
-add_image_data codegen $DIRNAME/GenAIExamples/CodeGen/docker Dockerfile
-add_image_data codegen-ui $DIRNAME/GenAIExamples/CodeGen/docker/ui docker/Dockerfile
-add_image_data codegen-react-ui $DIRNAME/GenAIExamples/CodeGen/docker/ui docker/Dockerfile.react
+add_image_data codegen $DIRNAME/GenAIExamples/CodeGen Dockerfile
+add_image_data codegen-ui $DIRNAME/GenAIExamples/CodeGen/ui docker/Dockerfile
+add_image_data codegen-react-ui $DIRNAME/GenAIExamples/CodeGen/ui docker/Dockerfile.react
 
-add_image_data codetrans $DIRNAME/GenAIExamples/CodeTrans/docker Dockerfile
-add_image_data codetrans-ui $DIRNAME/GenAIExamples/CodeTrans/docker/ui docker/Dockerfile
+add_image_data codetrans $DIRNAME/GenAIExamples/CodeTrans Dockerfile
+add_image_data codetrans-ui $DIRNAME/GenAIExamples/CodeTrans/ui docker/Dockerfile
 
-add_image_data docsum $DIRNAME/GenAIExamples/DocSum/docker Dockerfile
-add_image_data docsum-ui $DIRNAME/GenAIExamples/DocSum/docker/ui docker/Dockerfile
-add_image_data docsum-react-ui $DIRNAME/GenAIExamples/DocSum/docker/ui docker/Dockerfile.react
+add_image_data docsum $DIRNAME/GenAIExamples/DocSum Dockerfile
+add_image_data docsum-ui $DIRNAME/GenAIExamples/DocSum/ui docker/Dockerfile
+add_image_data docsum-react-ui $DIRNAME/GenAIExamples/DocSum/ui docker/Dockerfile.react
 # END of adding image data
 
 if [ $# -eq 0 ]; then
