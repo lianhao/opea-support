3fef1acc406aa7d8eb119f0d0f773d5c2ffb612a
Lianhao Lu <lianhao.lu@intel.com>
2025-05-19 17:42:18 +0800

    helm_genie: include reference helm chart in prompt

--- helm_genie/generate_helm_llm.py
+++ helm_genie/generate_helm_llm.py
@@ -82,7 +82,7 @@ def process_source_code(repo_path, code_dirs, exclusion_patterns, output, separa
 
 openai_chat= None
 
-def init_openai_client(endpoint, api_key="empty"):
+def init_openai_client(endpoint, api_key):
     global openai_chat
     r = JsonRequestsWrapper()
     model_info = r.get(f"{endpoint}/v1/models")
@@ -90,7 +90,7 @@ def init_openai_client(endpoint, api_key="empty"):
         openai_chat = ChatOpenAI(
           timeout=None,
           max_retries=0,
-          openai_api_key=api_key,
+          openai_api_key=api_key or "empty",
           openai_api_base=f"{endpoint}/v1",
           model_name=model_info["data"][0]["id"])
         logger.info(f"OpenAI client initialized with model: {model_info['data'][0]['id']}")
@@ -103,8 +103,13 @@ def main():
     parser.add_argument("--repo-url", type=str, help="URL of the git repository to process.",
                       default="https://github.com/opea-project/GenAIComps")
     parser.add_argument("--repo-path", type=str, help="Local path to the git repository.")
-    parser.add_argument("--llm-endpoint", type=str, help="Endpoint of the LLM API.")
     parser.add_argument("--code-dirs", type=str, nargs="+", help="Directories to process for source code.", default=["comps"])
+    parser.add_argument("--helm-repo-url", type=str, help="URL of the git repository to process.",
+                      default="https://github.com/opea-project/GenAIInfra")
+    parser.add_argument("--helm-repo-path", type=str, help="Local path to the git repository which contains the reference helm chart.")
+    parser.add_argument("--helm-dirs", type=str, nargs="+", help="Directories to the reference helm chart.", default=[])
+    parser.add_argument("--llm-endpoint", type=str, help="Endpoint of the LLM API.")
+    parser.add_argument("--llm-api-key", type=str, help="LLM API key.")
     parser.add_argument("--exclusion-patterns", type=str, nargs="+", help="Patterns to exclude files and directories.", default=["*/deployment"])
     parser.add_argument("--preemble", type=str, help="Preemble to add to the input of LLM")
     parser.add_argument("--epilog", type=str, help="Epilog to add to the input of LLM")
@@ -116,29 +121,51 @@ def main():
 
     if args.repo_path is None:
         args.repo_path = os.path.split(args.repo_url)[-1].replace(".git", "")
+    if args.helm_repo_path is None:
+        args.helm_repo_path = os.path.split(args.helm_repo_url)[-1].replace(".git", "")
 
-    init_openai_client(args.llm_endpoint)
+    init_openai_client(args.llm_endpoint, args.llm_api_key)
     update_repo(args.repo_url, args.repo_path)
+    update_repo(args.helm_repo_url, args.helm_repo_path)
 
+
+    default_source_preemble = "The following text represents a project with code. The structure of the text consists of sections beginning with ----, followed by a single line containing the file path and file name, and then a variable number of lines containing the file contents. The text representing the project ends when the symbols --END-- are encountered."
+    default_helm_preemble = "The following text represents a reference helm chart. The structure of the text consists of sections beginning with ----, followed by a single line containing the file path and file name, and then a variable number of lines containing the file contents. The text representing the helm chart ends when the symbols --END-- are encountered."
+    default_end_preemble = "Any further text beyond --END-- is meant to be interpreted as instructions using the aforementioned project as context."
     with io.StringIO() as output:
       if args.preemble:
         output.write(args.preemble + "\n")
-      else:
-        output.write("The following text represents a project with code. The structure of the text consists of sections beginning with ----, followed by a single line containing the file path and file name, and then a variable number of lines containing the file contents. The text representing the project ends when the symbols --END-- are encountered. Any further text beyond --END-- is meant to be interpreted as instructions using the aforementioned project as context.\n")
+      elif not args.helm_dirs:
+        output.write(default_source_preemble + default_end_preemble + "\n")
+      else: # src with reference helm
+        output.write(default_source_preemble + "\n")
+
       if not process_source_code(args.repo_path, args.code_dirs, args.exclusion_patterns, output):
-        logger.warning("No files were processed. Please check the exclusion patterns or the repository contents.")
+        logger.warning("No files were processed. Please check the exclusion patterns or the code repository contents.")
         return 0
       output.write("\n--END--\n")
+      if args.helm_dirs:
+        output.write(default_helm_preemble + default_end_preemble + "\n")
+        if not process_source_code(args.helm_repo_path, args.helm_dirs, args.exclusion_patterns, output):
+          logger.warning("No files were processed. Please check the exclusion patterns or the helm repository contents.")
+          return 0
+        output.write("\n--END--\n")
+
       if args.epilog:
          output.write("\n" + args.epilog + "\n")
       else:
-        output.write("\nPlease generate a helm chart for based on the above code. The helm chart should be compatible with Kubernetes and should include all necessary files and configurations. The helm chart should include the user configurations in the helm chart's values.yaml file with comment indicating the purpose of each configuration item. These user configurations will be loaded through a Kuberenets configmap into the pods. Please don't show the reasoning steps and only show the final helm chart's content. The final helm chart's content begins with '----HELM START----', ends with '----HELM END----' and each file begins with '----'.\n")
+        output.write("\n" + "Please generate a helm chart for the above code using the above reference helm chart. The generated helm chart should be compatible with Kubernetes and should include all necessary files and configurations. The generated helm chart should follow the same convention as the above reference helm chart, to include the user configurations in the helm chart's values.yaml file with comment indicating the purpose of each configuration item. These user configurations will be loaded throught a Kubernetes configmap into the pods. The config map should only contain the valid configuration items for the user specified backend. The generated helm chart's content begins with '----HELM START----', ends with '----HELM END----' and each file begins with '----'." + "\n")
+
       # call LLM
       logger.info("Calling LLM with the processed source code.")
       content=str(output.getvalue())
       if args.verbose:
         logger.info(f"Content to LLM: \n{content}")
-      response = openai_chat.invoke([("human", content)])
+      response = openai_chat.invoke([("human", content)],
+                                temperature=0.01,
+                                frequency_penalty=0.0,
+                                presence_penalty=0.0, )
+      logger.info("LLM response received.")
       logger.info(f"Response content: \n{response.content}")
       logger.info(f"usage metadata: {response.usage_metadata}")
 
